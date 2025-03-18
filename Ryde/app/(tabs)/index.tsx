394d1872987ecc578c0c51becb01f9b80daf02ca
fleo1977 <leonardofilho1977@gmail.com>
2025-03-18 09:03:10 -0400

    backup

--- Ryde/app/(tabs)/index.tsx
+++ Ryde/app/(tabs)/index.tsx
@@ -1,12 +1,11 @@
-import "nativewind";
-import { View, Text, StatusBar } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context'
+import { Text, StatusBar } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function HomeScreen() {
-  return (
-      <SafeAreaView className={"flex-1 items-center justify-center bg-white"}>
-        <Text className={"text-red-500"}>Uber Clone</Text>
-        <StatusBar style="auto" />
-      </SafeAreaView>
-  );
+    return (
+        <SafeAreaView className="flex-1 items-center justify-center bg-white">
+            <Text className="text-red-500">Uber Clone</Text>
+            <StatusBar style="auto" />
+        </SafeAreaView>
+    );
 }
